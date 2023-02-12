5b7bfa61a2a624861d7b5dadf49af8ab48a3e9dc
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2023-02-12 10:42:44 -0300

    FeatÇ recuperando valores das caixa de texto.

--- componentes-basicos/ComponentesBasicos/app/src/main/java/com/cursoandroid/componentesbasicos/MainActivity.java
+++ componentes-basicos/ComponentesBasicos/app/src/main/java/com/cursoandroid/componentesbasicos/MainActivity.java
@@ -3,12 +3,43 @@ package com.cursoandroid.componentesbasicos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText campoNome;
+    private TextInputEditText campoEmail;
+    private TextView textoResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        campoNome = findViewById(R.id.editNome);
+        campoEmail = findViewById(R.id.editEmail);
+        textoResultado = findViewById(R.id.textResultado);
+
     }
+
+    public void enviar(View view){
+
+        String nome = campoNome.getText().toString();
+        String email = campoEmail.getText().toString();
+
+        textoResultado.setText( "nome: " + nome + " email: " + email );
+
+    }
+
+    public void limpar(View view){
+
+        campoNome.setText("");
+        campoEmail.setText("");
+
+    }
+
 }
