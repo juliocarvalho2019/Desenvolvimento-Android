a5fab8d91457e3d01496f73910b36cfb1e316a7d
juliocarvalho2019 <juliocarvalhocbq@hotmail.com>
2023-02-12 10:46:02 -0300

    Feat: caixas de seleção.

--- componentes-basicos/ComponentesBasicos/app/src/main/java/com/cursoandroid/componentesbasicos/MainActivity.java
+++ componentes-basicos/ComponentesBasicos/app/src/main/java/com/cursoandroid/componentesbasicos/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,23 +16,56 @@ public class MainActivity extends AppCompatActivity {
     private TextInputEditText campoEmail;
     private TextView textoResultado;
 
+    private CheckBox checkVerde, checkBranco, checkVermelho;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        campoNome = findViewById(R.id.editNome);
-        campoEmail = findViewById(R.id.editEmail);
-        textoResultado = findViewById(R.id.textResultado);
+        campoNome       = findViewById(R.id.editNome);
+        campoEmail      = findViewById(R.id.editEmail);
+        textoResultado  = findViewById(R.id.textResultado);
+
+        //Checkbox
+        checkVerde      = findViewById(R.id.checkVerde);
+        checkBranco     = findViewById(R.id.checkBranco);
+        checkVermelho   = findViewById(R.id.checkVermelho);
+
+    }
+
+    public void checkbox(){
+
+        String texto = "";
+
+        if( checkVerde.isChecked() ){
+            //String corSelecionada = checkVerde.getText().toString();
+            //texto = corSelecionada;
+            texto = "Verde selecionado - ";
+        }
+
+        if( checkBranco.isChecked() ){
+            texto = texto + "Branco selecionado - ";
+        }
+
+        if( checkVermelho.isChecked() ){
+            texto = texto + "Vermelho selecionado - ";
+        }
+
+        textoResultado.setText( texto );
 
     }
 
     public void enviar(View view){
 
+        checkbox();
+
+        /*
         String nome = campoNome.getText().toString();
         String email = campoEmail.getText().toString();
 
         textoResultado.setText( "nome: " + nome + " email: " + email );
+         */
 
     }
 
